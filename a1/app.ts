9ad0da06b4bc4a892f5bc91cc4c166a4affd5e82
Daniel Melo <o.danielworkmelo@gmail.com>
2023-02-20 20:48:31 +0000

    A5

--- a1/app.ts
+++ a1/app.ts
@@ -1,22 +1,5 @@
-const numero: number = 15;
-if (numero > 15) {
-    console.log('Numero maior que 15');
-} else if (numero === 15) {
-    console.log('Numero igual 15');
-} else {
-    console.log('Numero menor que 15');
-}
+const array: Array<any> = [1, 2, 3];
+const stringArray: string[] = ['1', '2', '3'];
 
-const typeUser = {
-    admin: 'Seja bem vindo',
-    student: 'Você é um estudante',
-    viewer: 'Você pode visualizar'
-}
-
-const usuario = 'admin';
-
-function validateUser(user:string){
-    console.log(typeUser[user as keyof typeof typeUser])
-}
-
-validateUser(usuario)
+// console.log(array[1]);
+// console.log(stringArray.length);
