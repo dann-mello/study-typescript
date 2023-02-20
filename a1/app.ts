bd9f0870fcc744ed8b081ba5a3dd0c4abd953b52
Daniel Melo <o.danielworkmelo@gmail.com>
2023-02-20 21:09:19 +0000

    A6

--- a1/app.ts
+++ a1/app.ts
@@ -1,5 +1,24 @@
-const array: Array<any> = [1, 2, 3];
+const array: Array<any> = [1, 2, 3, 4];
 const stringArray: string[] = ['1', '2', '3'];
 
 // console.log(array[1]);
 // console.log(stringArray.length);
+
+// const buscaNum = array.find((num) => {
+//     return num === 4;
+// })
+
+// console.log(buscaNum);
+
+array.forEach((num) => {
+   if(num > 2 && num % 2 === 0){
+    console.log(num * 2)
+   }
+})
+
+array.map((num) => {
+   if(num > 2 && num % 2 === 0){
+    console.log(num * 2)
+   }
+})
+
