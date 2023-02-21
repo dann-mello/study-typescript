be6284fd17ea9b772e919827d73dec90eaa2b1a3
Daniel Melo <o.danielworkmelo@gmail.com>
2023-02-21 10:25:15 +0000

    A7

--- a1/app.ts
+++ a1/app.ts
@@ -1,20 +1,83 @@
-//OO
-class User {
-    name: string = 'Daniel'
-    age: number = 19
+//Banco
 
-    constructor(name: string, age:number){
+//Absttração
+//name, accountNumber
+//depositar, sacar
+
+// class Account {
+//     name: string
+//     accountNumber: number
+
+//     constructor(name: string, accountNumber: number) {
+//         this.name = name;
+//         this.accountNumber = accountNumber;
+//     }
+
+//     doposit = () => {
+//         console.log('Você despositou !')
+//     }
+
+//     withdraw = () => {
+//         console.log('Você sacou !')
+//     }
+// }
+
+// //OO Herança
+// class AdminAccount extends Account{
+//     balance:number
+
+//     constructor(name: string, accountNumber: number){
+//         super(name, accountNumber)
+//         this.balance = 20;
+//     }
+
+//     getBalance = () =>{
+//         console.log(this.balance)
+//     }
+// }
+
+// const account: Account = new Account('Daniel', 154289);
+// const admin: AdminAccount = new AdminAccount('Joao', 154289);
+
+// console.log(account)
+// console.log(admin)
+
+
+
+
+//OO Classes Abstratas
+abstract class Account {
+    name: string
+    accountNumber: number
+    balance:number = 20
+
+    constructor(name: string, accountNumber: number) {
         this.name = name;
-        this.age = age;
+        this.accountNumber = accountNumber;
     }
 
-    showName = () => {
-        console.log(this.name)
+    doposit = () => {
+        console.log('Você despositou !')
+    }
+
+    withdraw = () => {
+        console.log('Você sacou !')
+    }
+
+    getBalance = () =>{
+        console.log(this.balance)
     }
 }
 
-const user1 = new User('Daniel', 19);
-user1.showName();
+//Contas fisicas
+class People extends Account{
+    doc_id:number
 
-const user2 = new User('Joao', 129);
-user2.showName();
+    constructor(name: string, doc_id:number, accountNumber: number){
+        super(name, accountNumber)
+        this.doc_id = doc_id;
+    }
+}
+
+const people1: People = new People('Daniel', 3, 567896);
+console.log(people1)
