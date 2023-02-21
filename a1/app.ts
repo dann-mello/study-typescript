dcbd88b19f979ea743766e55e029a027752580bf
Daniel Melo <o.danielworkmelo@gmail.com>
2023-02-21 11:21:12 +0000

    A9

--- a1/app.ts
+++ a1/app.ts
@@ -1,7 +1,9 @@
 import { PeopleAccount } from '../class/PeopleAccount'
 import { CompanyAccount } from '../class/CompanyAccount'
 
-const company1: CompanyAccount = new CompanyAccount('Google', 31)
+const company1: CompanyAccount = new CompanyAccount('Daniel', 31)
 const people1: PeopleAccount = new PeopleAccount('Google', 1, 31)
-company1.deposit()
+// console.log(people1)
+// people1.setName('Epaminondas');
+console.log(people1)
 people1.deposit()
--- class/Account.ts
+++ class/Account.ts
@@ -1,7 +1,8 @@
 export abstract class Account {
-    name: string
+    private name: string
     accountNumber: number
     balance: number = 20
+    private status: boolean = true
 
     constructor(name: string, accountNumber: number) {
         this.name = name;
@@ -9,11 +10,30 @@ export abstract class Account {
     }
 
     deposit = (): void => {
-        console.log('Você despositou !')
+        if(this.validateStatus()){
+            console.log('Voce depositou !')
+        }
     }
 
     withdraw = (): void => {
         console.log('Você sacou !')
+    }
+
+    public setName = (name:string):void => {
+        this.name = name;
+        console.log('Nome alterado com sucesso !')
+    }
+
+    getName = ():string => {
+        return this.name
+    }
+
+    private validateStatus = ():boolean => {
+        if(this.status){
+            return this.status
+        }
+
+        throw new Error('Conta inválida')
     }
 
     getBalance = (): void => {
